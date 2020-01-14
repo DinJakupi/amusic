adddb69e417fef846b5ba27815f40d46f372b7e6
Din Jakupi <dinjakupi@protonmail.com>
2020-01-14 18:50:41 +0100

    created three Buttons (back,play,skip) and there onclick event

--- amusic/amusic/MainActivity.cs
+++ amusic/amusic/MainActivity.cs
@@ -3,10 +3,11 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Views;
 
 namespace amusic
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "amusic", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -15,6 +16,26 @@ namespace amusic
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Button playButton = FindViewById<Button>(Resource.Id.playButton);
+            Button backButton = FindViewById<Button>(Resource.Id.backButton);
+            Button skipButton = FindViewById<Button>(Resource.Id.skipButton);
+
+            playButton.Click += (sender, e) =>
+            {
+                //code to play music
+            };
+
+            backButton.Click += (sender, e) =>
+            {
+                //code to go back an song
+            };
+
+            skipButton.Click += (sender, e) =>
+            {
+                //code to  skip a song
+            }; 
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
